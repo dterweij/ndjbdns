9119bbb36041a227ac1be295de542fd92d9c760b
P J P <pj.pandit@yahoo.co.in>
2012-12-08 23:48:28 +0530

    Add pickdns and picdns-data programs.
    Updated pickdns and pickdns-data programs to format their sources
    for better readability. Removed unwanted/confusing definitions.
    
    Updated pickdns program to read a configuration file, run as a
    daemon and also to write debug output to a pickdns.log file.
    
    Introduced a new pickdns-conf file which is used to generate real
    pickdns.conf file, which is then installed under /etc/ndjbdns
    directory.
    
    Modified Makefiles.am to install pickdns, pickdns-data and
    pickdns.conf files under their correct location, and bundle these
    into distribution tar file.

--- pickdns-data.c
+++ pickdns-data.c
@@ -1,99 +1,154 @@
+/*
+ * pickdns-data.c: This file is part of the `djbdns' project, originally
+ * written by Dr. D J Bernstein and later released under public-domain
+ * since late December 2007 (http://cr.yp.to/distributors.html).
+ *
+ * Copyright (C) 2009 - 2012 Prasad J Pandit
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of GNU General Public License as published by Free
+ * Software Foundation; either version 2 of the license or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <err.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-#include "buffer.h"
-#include "cdb_make.h"
-#include "open.h"
-#include "alloc.h"
-#include "gen_allocdefs.h"
-#include "stralloc.h"
-#include "getln.h"
-#include "case.h"
-#include "strerr.h"
-#include "str.h"
-#include "byte.h"
-#include "scan.h"
+
 #include "fmt.h"
 #include "ip4.h"
 #include "dns.h"
+#include "str.h"
+#include "byte.h"
+#include "scan.h"
+#include "case.h"
+#include "open.h"
+#include "alloc.h"
+#include "getln.h"
+#include "strerr.h"
+#include "buffer.h"
+#include "cdb_make.h"
+#include "stralloc.h"
+#include "gen_allocdefs.h"
 
-#define FATAL "pickdns-data: fatal: "
 
-void nomem(void)
+struct address
 {
-  strerr_die2x(111,FATAL,"out of memory");
-}
+    char *name;
+    unsigned int namelen;
 
-void ipprefix_cat(stralloc *out,char *s)
+    char ip[4];
+    char location[2];
+};
+
+void
+ipprefix_cat (stralloc *out, char *s)
 {
-  unsigned long u;
-  char ch;
-  unsigned int j;
+    char ch = 0;
+    unsigned int j = 0;
+    unsigned long u = 0;
 
-  for (;;)
-    if (*s == '.')
-      ++s;
-    else {
-      j = scan_ulong(s,&u);
-      if (!j) return;
-      s += j;
-      ch = u;
-      if (!stralloc_catb(out,&ch,1)) nomem();
+    for (;;)
+    {
+        if (*s == '.')
+          ++s;
+        else
+        {
+            j = scan_ulong (s, &u);
+            if (!j)
+                return;
+
+            s += j;
+            ch = u;
+            if (!stralloc_catb (out, &ch, 1))
+                err (-1, "could not allocate enough memory");
+        }
     }
 }
 
-struct address {
-  char *name;
-  unsigned int namelen;
-  char ip[4];
-  char location[2];
-} ;
-
-int address_diff(struct address *p,struct address *q)
+int
+address_diff (struct address *p, struct address *q)
 {
-  int r;
+    int r = 0;
 
-  r = byte_diff(p->location,2,q->location);
-  if (r < 0) return -1;
-  if (r > 0) return 1;
-  if (p->namelen < q->namelen) return -1;
-  if (p->namelen > q->namelen) return 1;
-  return case_diffb(p->name,p->namelen,q->name);
+    r = byte_diff (p->location, 2, q->location);
+    if (r < 0)
+      return -1;
+    if (r > 0)
+      return 1;
+    if (p->namelen < q->namelen)
+      return -1;
+    if (p->namelen > q->namelen)
+      return 1;
+
+    return case_diffb (p->name, p->namelen, q->name);
 }
 
-void address_sort(struct address *z,unsigned int n)
+void
+address_sort (struct address *z, unsigned int n)
 {
-  unsigned int i;
-  unsigned int j;
-  unsigned int p;
-  unsigned int q;
-  struct address t;
+    unsigned int i = 0;
+    unsigned int j = 0;
+    unsigned int p = 0;
+    unsigned int q = 0;
+    struct address t;
 
-  i = j = n;
-  --z;
+    i = j = n;
+    --z;
 
-  while (j > 1) {
-    if (i > 1) { --i; t = z[i]; }
-    else { t = z[j]; z[j] = z[i]; --j; }
-    q = i;
-    while ((p = q * 2) < j) {
-      if (address_diff(&z[p + 1],&z[p]) >= 0) ++p;
-      z[q] = z[p]; q = p;
+    while (j > 1)
+    {
+        if (i > 1)
+        {
+            --i;
+            t = z[i];
+        }
+        else
+        {
+            t = z[j];
+            z[j] = z[i];
+            --j;
+        }
+
+        q = i;
+        while ((p = q * 2) < j)
+        {
+            if (address_diff (&z[p + 1], &z[p]) >= 0)
+                ++p;
+            z[q] = z[p];
+            q = p;
+        }
+
+        if (p == j)
+        {
+            z[q] = z[p];
+            q = p;
+        }
+        while ((q > i) && (address_diff (&t, &z[p = q/2]) > 0))
+        {
+            z[q] = z[p];
+            q = p;
+        }
+        z[q] = t;
     }
-    if (p == j) {
-      z[q] = z[p]; q = p;
-    }
-    while ((q > i) && (address_diff(&t,&z[p = q/2]) > 0)) {
-      z[q] = z[p]; q = p;
-    }
-    z[q] = t;
-  }
 }
 
-GEN_ALLOC_typedef(address_alloc,struct address,s,len,a)
-GEN_ALLOC_readyplus(address_alloc,struct address,s,len,a,i,n,x,30,address_alloc_readyplus)
-GEN_ALLOC_append(address_alloc,struct address,s,len,a,i,n,x,30,address_alloc_readyplus,address_alloc_append)
+GEN_ALLOC_typedef(address_alloc, struct address, s, len, a)
+GEN_ALLOC_readyplus(address_alloc, struct address,
+                    s, len, a, i, n, x, 30, address_alloc_readyplus)
+GEN_ALLOC_append(address_alloc, struct address,
+        s, len, a, i, n, x, 30,address_alloc_readyplus,address_alloc_append)
 
 static address_alloc x;
 
@@ -106,124 +161,158 @@ struct cdb_make cdb;
 static stralloc key;
 static stralloc result;
 
-static stralloc line;
 int match = 1;
+static stralloc line;
 unsigned long linenum = 0;
 
 #define NUMFIELDS 3
+char strnum[FMT_ULONG];
 static stralloc f[NUMFIELDS];
 
-char strnum[FMT_ULONG];
+void
+syntaxerror (const char *why)
+{
+    strnum[fmt_ulong (strnum, linenum)] = 0;
+    err (-1, "unable to parse data line: %s: %s", strnum, why);
+}
 
-void syntaxerror(const char *why)
-{
-  strnum[fmt_ulong(strnum,linenum)] = 0;
-  strerr_die4x(111,FATAL,"unable to parse data line ",strnum,why);
-}
-void die_datatmp(void)
-{
-  strerr_die2sys(111,FATAL,"unable to create data.tmp: ");
-}
 
 int main()
 {
-  struct address t;
-  int i;
-  int j;
-  int k;
-  char ch;
+    char ch = 0;
+    struct address t;
+    int i = 0, j= 0, k = 0;
 
-  umask(022);
+    umask(022);
+    if (!address_alloc_readyplus (&x, 0))
+        err (-1, "could not allocate enough memory");
 
-  if (!address_alloc_readyplus(&x,0)) nomem();
+    fd = open_read("data");
+    if (fd == -1)
+        err (-1, "could not open file: `data'");
+    buffer_init (&b, buffer_unixread, fd, bspace, sizeof bspace);
 
-  fd = open_read("data");
-  if (fd == -1) strerr_die2sys(111,FATAL,"unable to open data: ");
-  buffer_init(&b,buffer_unixread,fd,bspace,sizeof bspace);
+    fdcdb = open_trunc ("data.tmp");
+    if (fdcdb == -1)
+        err (-1, "could not create file: `data.tmp'");
+    if (cdb_make_start(&cdb, fdcdb) == -1)
+        err (-1, "could not create file: `data.tmp'");
 
-  fdcdb = open_trunc("data.tmp");
-  if (fdcdb == -1) die_datatmp();
-  if (cdb_make_start(&cdb,fdcdb) == -1) die_datatmp();
+    while (match)
+    {
+        ++linenum;
+        if (getln (&b, &line, &match, '\n') == -1)
+            err (-1, "could not read line");
 
-  while (match) {
-    ++linenum;
-    if (getln(&b,&line,&match,'\n') == -1)
-      strerr_die2sys(111,FATAL,"unable to read line: ");
+        while (line.len)
+        {
+            ch = line.s[line.len - 1];
+            if ((ch != ' ') && (ch != '\t') && (ch != '\n'))
+                break;
+            --line.len;
+        }
+        if (!line.len)
+            continue;
 
-    while (line.len) {
-      ch = line.s[line.len - 1];
-      if ((ch != ' ') && (ch != '\t') && (ch != '\n')) break;
-      --line.len;
+        j = 1;
+        for (i = 0; i < NUMFIELDS; ++i)
+        {
+            if (j >= line.len)
+            {
+                if (!stralloc_copys(&f[i],""))
+                    err (-1, "could not allocate enough memory");
+            }
+            else
+            {
+                k = byte_chr (line.s + j, line.len - j, ':');
+                if (!stralloc_copyb (&f[i], line.s + j, k))
+                    err (-1, "could not allocate enough memory");
+                j += k + 1;
+            }
+        }
+
+        switch (line.s[0])
+        {
+        default:
+            syntaxerror(": unrecognized leading character");
+
+        case '#':
+        case '-':
+            break;
+
+        case '+':
+            byte_zero (&t, sizeof t);
+            if (!dns_domain_fromdot (&t.name, f[0].s, f[0].len))
+                err (-1, "could not allocate enough memory");
+
+            t.namelen = dns_domain_length (t.name);
+            case_lowerb (t.name, t.namelen);
+
+            if (!stralloc_0 (&f[1]))
+                err (-1, "could not allocate enough memory");
+            if (!ip4_scan (f[1].s, t.ip))
+                syntaxerror(": malformed IP address");
+            if (!stralloc_0 (&f[2]))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_0 (&f[2]))
+                err (-1, "could not allocate enough memory");
+
+            byte_copy (t.location, 2, f[2].s);
+            if (!address_alloc_append (&x, &t))
+                err (-1, "could not allocate enough memory");
+            break;
+
+        case '%':
+            if (!stralloc_0 (&f[0]))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_0 (&f[0]))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_copyb (&result, f[0].s, 2))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_0 (&f[1]))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_copys (&key, "%"))
+                err (-1, "could not allocate enough memory");
+            ipprefix_cat (&key, f[1].s);
+            if (cdb_make_add (&cdb, key.s, key.len, result.s, result.len) == -1)
+                err (-1, "could not access file: `data.tmp'");
+            break;
+        }
     }
-    if (!line.len) continue;
+    close (fd);
+    address_sort (x.s, x.len);
 
-    j = 1;
-    for (i = 0;i < NUMFIELDS;++i) {
-      if (j >= line.len) {
-	if (!stralloc_copys(&f[i],"")) nomem();
-      }
-      else {
-        k = byte_chr(line.s + j,line.len - j,':');
-	if (!stralloc_copyb(&f[i],line.s + j,k)) nomem();
-	j += k + 1;
-      }
+    i = 0;
+    while (i < x.len)
+    {
+        for (j = i + 1; j < x.len; ++j)
+            if (address_diff (x.s + i, x.s + j))
+                break;
+
+        if (!stralloc_copys (&key, "+"))
+            err (-1, "could not allocate enough memory");
+        if (!stralloc_catb (&key, x.s[i].location, 2))
+            err (-1, "could not allocate enough memory");
+        if (!stralloc_catb (&key, x.s[i].name, x.s[i].namelen))
+            err (-1, "could not allocate enough memory");
+        if (!stralloc_copys (&result, ""))
+            err (-1, "could not allocate enough memory");
+
+        while (i < j)
+            if (!stralloc_catb (&result, x.s[i++].ip, 4))
+                err (-1, "could not allocate enough memory");
+        if (cdb_make_add (&cdb, key.s, key.len, result.s, result.len) == -1)
+            err (-1, "could not access file: `data.tmp'");
     }
 
-    switch(line.s[0]) {
-      default:
-	syntaxerror(": unrecognized leading character");
-      case '#':
-	break;
-      case '-':
-        break;
-      case '+':
-	byte_zero(&t,sizeof t);
-	if (!dns_domain_fromdot(&t.name,f[0].s,f[0].len)) nomem();
-	t.namelen = dns_domain_length(t.name);
-	case_lowerb(t.name,t.namelen);
-	if (!stralloc_0(&f[1])) nomem();
-	if (!ip4_scan(f[1].s,t.ip)) syntaxerror(": malformed IP address");
-	if (!stralloc_0(&f[2])) nomem();
-	if (!stralloc_0(&f[2])) nomem();
-	byte_copy(t.location,2,f[2].s);
-	if (!address_alloc_append(&x,&t)) nomem();
-	break;
-      case '%':
-	if (!stralloc_0(&f[0])) nomem();
-	if (!stralloc_0(&f[0])) nomem();
-	if (!stralloc_copyb(&result,f[0].s,2)) nomem();
-	if (!stralloc_0(&f[1])) nomem();
-	if (!stralloc_copys(&key,"%")) nomem();
-	ipprefix_cat(&key,f[1].s);
-        if (cdb_make_add(&cdb,key.s,key.len,result.s,result.len) == -1)
-          die_datatmp();
-	break;
-    }
-  }
-
-  close(fd);
-  address_sort(x.s,x.len);
-
-  i = 0;
-  while (i < x.len) {
-    for (j = i + 1;j < x.len;++j)
-      if (address_diff(x.s + i,x.s + j))
-	break;
-    if (!stralloc_copys(&key,"+")) nomem();
-    if (!stralloc_catb(&key,x.s[i].location,2)) nomem();
-    if (!stralloc_catb(&key,x.s[i].name,x.s[i].namelen)) nomem();
-    if (!stralloc_copys(&result,"")) nomem();
-    while (i < j)
-      if (!stralloc_catb(&result,x.s[i++].ip,4)) nomem();
-    if (cdb_make_add(&cdb,key.s,key.len,result.s,result.len) == -1)
-      die_datatmp();
-  }
-
-  if (cdb_make_finish(&cdb) == -1) die_datatmp();
-  if (fsync(fdcdb) == -1) die_datatmp();
-  if (close(fdcdb) == -1) die_datatmp(); /* NFS stupidity */
-  if (rename("data.tmp","data.cdb") == -1)
-    strerr_die2sys(111,FATAL,"unable to move data.tmp to data.cdb: ");
+    if (cdb_make_finish (&cdb) == -1)
+        err (-1, "could not access file: `data.tmp'");
+    if (fsync (fdcdb) == -1)
+        err (-1, "could not access file: `data.tmp'");
+    if (close (fdcdb) == -1)
+        err (-1, "could not access file: `data.tmp'"); /* NFS stupidity */
+    if (rename("data.tmp","data.cdb") == -1)
+        err (-1, "could not move `data.tmp' to `data.cdb'");
 
     return 0;
 }
--- pickdns.c
+++ pickdns.c
@@ -1,101 +1,161 @@
-#include <unistd.h>
+/*
+ * pickdns.c: This file is part of the `djbdns' project, originally written
+ * by Dr. D J Bernstein and later released under public-domain since late
+ * December 2007 (http://cr.yp.to/distributors.html).
+ *
+ * Copyright (C) 2009 - 2012 Prasad J Pandit
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of GNU General Public License as published by Free
+ * Software Foundation; either version 2 of the license or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#include "dns.h"
+#include "cdb.h"
 #include "byte.h"
 #include "case.h"
-#include "dns.h"
 #include "open.h"
-#include "cdb.h"
+#include "common.h"
 #include "response.h"
 
-const char *fatal = "pickdns: fatal: ";
-const char *starting = "starting pickdns\n";
+#define PIDFILE "/var/run/pickdns.pid"
+#define LOGFILE "/var/log/pickdns.log"
+#define CFGFILE SYSCONFDIR"/ndjbdns/pickdns.conf"
 
-static char seed[128];
-
-void initialize(void)
-{
-  dns_random_init(seed);
-}
+extern short mode;
+enum op_mode { DAEMON = 1, DEBUG = 2 };
 
 static struct cdb c;
 static char key[258];
+static char seed[128];
 static char data[512];
 
-static int doit(char *q,char qtype[2],char ip[4])
+void
+initialize (void)
 {
-  int r;
-  uint32 dlen;
-  unsigned int qlen;
-  int flaga;
-  int flagmx;
+    read_conf (CFGFILE);
 
-  qlen = dns_domain_length(q);
-  if (qlen > 255) return 0; /* impossible */
+    if (mode & DAEMON)
+    {
+        /* redirect stdout & stderr to a log file */
+        redirect_to_log (LOGFILE);
 
-  flaga = byte_equal(qtype,2,DNS_T_A);
-  flagmx = byte_equal(qtype,2,DNS_T_MX);
-  if (byte_equal(qtype,2,DNS_T_ANY)) flaga = flagmx = 1;
-  if (!flaga && !flagmx) goto REFUSE;
-
-  key[0] = '%';
-  byte_copy(key + 1,4,ip);
-
-  r = cdb_find(&c,key,5);
-  if (!r) r = cdb_find(&c,key,4);
-  if (!r) r = cdb_find(&c,key,3);
-  if (!r) r = cdb_find(&c,key,2);
-  if (r == -1) return 0;
-
-  key[0] = '+';
-  byte_zero(key + 1,2);
-  if (r && (cdb_datalen(&c) == 2))
-    if (cdb_read(&c,key + 1,2,cdb_datapos(&c)) == -1) return 0;
-
-  byte_copy(key + 3,qlen,q);
-  case_lowerb(key + 3,qlen + 3);
-
-  r = cdb_find(&c,key,qlen + 3);
-  if (!r) {
-    byte_zero(key + 1,2);
-    r = cdb_find(&c,key,qlen + 3);
-  }
-  if (!r) goto REFUSE;
-  if (r == -1) return 0;
-  dlen = cdb_datalen(&c);
-
-  if (dlen > 512) dlen = 512;
-  if (cdb_read(&c,data,dlen,cdb_datapos(&c)) == -1) return 0;
-
-  if (flaga) {
-    dns_sortip(data,dlen);
-    if (dlen > 12) dlen = 12;
-    while (dlen >= 4) {
-      dlen -= 4;
-      if (!response_rstart(q,DNS_T_A,5)) return 0;
-      if (!response_addbytes(data + dlen,4)) return 0;
-      response_rfinish(RESPONSE_ANSWER);
+        write_pid (PIDFILE);
     }
-  }
 
-  return 1;
-
-
-  REFUSE:
-  response[2] &= ~4;
-  response[3] &= ~15;
-  response[3] |= 5;
-  return 1;
+    dns_random_init (seed);
 }
 
-int respond(char *q,char qtype[2],char ip[4])
+static int
+doit (char *q, char qtype[2], char ip[4])
 {
-  int fd;
-  int result;
+    int r = 0;
+    unsigned int dlen = 0;
+    unsigned int qlen = 0;
+    int flaga = 0, flagmx = 0;
 
-  fd = open_read("data.cdb");
-  if (fd == -1) return 0;
-  cdb_init(&c,fd);
-  result = doit(q,qtype,ip);
-  cdb_free(&c);
-  close(fd);
-  return result;
+    qlen = dns_domain_length (q);
+    if (qlen > 255)
+        return 0; /* impossible */
+
+    flaga = byte_equal (qtype, 2, DNS_T_A);
+    flagmx = byte_equal(qtype, 2, DNS_T_MX);
+    if (byte_equal (qtype, 2, DNS_T_ANY))
+        flaga = flagmx = 1;
+    if (!flaga && !flagmx)
+        goto REFUSE;
+
+    key[0] = '%';
+    byte_copy (key + 1, 4, ip);
+
+    r = cdb_find (&c, key, 5);
+    if (!r)
+        r = cdb_find (&c, key, 4);
+    if (!r)
+        r = cdb_find (&c, key, 3);
+    if (!r)
+        r = cdb_find (&c, key, 2);
+    if (r == -1)
+        return 0;
+
+    key[0] = '+';
+    byte_zero (key + 1, 2);
+    if (r && (cdb_datalen (&c) == 2))
+        if (cdb_read (&c, key + 1, 2, cdb_datapos (&c)) == -1)
+            return 0;
+
+    byte_copy (key + 3, qlen, q);
+    case_lowerb (key + 3, qlen + 3);
+
+    r = cdb_find (&c, key, qlen + 3);
+    if (!r)
+    {
+        byte_zero (key + 1, 2);
+        r = cdb_find (&c, key, qlen + 3);
+    }
+    if (!r)
+        goto REFUSE;
+    if (r == -1)
+        return 0;
+
+    dlen = cdb_datalen (&c);
+    if (dlen > 512)
+        dlen = 512;
+    if (cdb_read (&c, data, dlen, cdb_datapos (&c)) == -1)
+        return 0;
+
+    if (flaga)
+    {
+        dns_sortip (data, dlen);
+        if (dlen > 12)
+            dlen = 12;
+        while (dlen >= 4)
+        {
+            dlen -= 4;
+            if (!response_rstart (q, DNS_T_A, 5))
+                return 0;
+            if (!response_addbytes (data + dlen, 4))
+                return 0;
+            response_rfinish (RESPONSE_ANSWER);
+        }
+    }
+
+    return 1;
+
+REFUSE:
+
+    response[2] &= ~4;
+    response[3] &= ~15;
+    response[3] |= 5;
+
+    return 1;
+}
+
+int
+respond (char *q, char qtype[2], char ip[4])
+{
+    int fd;
+    int result;
+
+    fd = open_read ("data.cdb");
+    if (fd == -1)
+        return 0;
+
+    cdb_init (&c, fd);
+    result = doit (q, qtype, ip);
+    cdb_free (&c);
+
+    close (fd);
+    return result;
 }
