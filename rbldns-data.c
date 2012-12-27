86a626a964e21f66b167ba84aacdd72254083d19
P J P <pj.pandit@yahoo.co.in>
2012-12-27 18:37:59 +0530

    Build rbldns & rbldns-data servers.
    Changed rbldns and rbldns-data tools format the source. Updated
    Makefile to build and install the two programs.

--- rbldns-data.c
+++ rbldns-data.c
@@ -1,127 +1,160 @@
+/*
+ * rbldns-data.c: This file is part of the `ndjbdns' project, originally
+ * written by Dr. D J Bernstein and later released under public-domain since
+ * late December 2007 (http://cr.yp.to/distributors.html).
+ *
+ * Copyright (C) 2009 - 2012 Prasad J Pandit
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of GNU General Public License as published by Free
+ * Software Foundation; either version 2 of the license or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#include <err.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <unistd.h>
-#include "buffer.h"
-#include "cdb_make.h"
-#include "open.h"
-#include "stralloc.h"
-#include "getln.h"
-#include "strerr.h"
-#include "byte.h"
-#include "scan.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "fmt.h"
 #include "ip4.h"
+#include "byte.h"
+#include "open.h"
+#include "scan.h"
+#include "getln.h"
+#include "strerr.h"
+#include "buffer.h"
+#include "cdb_make.h"
+#include "stralloc.h"
 
-#define FATAL "rbldns-data: fatal: "
-
-void nomem(void)
-{
-  strerr_die2x(111,FATAL,"out of memory");
-}
-
-int fd;
 buffer b;
+int fd = 0;
 char bspace[1024];
 
-int fdcdb;
+int fdcdb = 0;
 struct cdb_make cdb;
 static stralloc tmp;
 
-static stralloc line;
 int match = 1;
+static stralloc line;
 unsigned long linenum = 0;
 
-char strnum[FMT_ULONG];
-
-void syntaxerror(const char *why)
+int
+main (int argc, char *argv[])
 {
-  strnum[fmt_ulong(strnum,linenum)] = 0;
-  strerr_die4x(111,FATAL,"unable to parse data line ",strnum,why);
-}
-void die_datatmp(void)
-{
-  strerr_die2sys(111,FATAL,"unable to create data.tmp: ");
-}
+    char ip[4], ch = 0;
+    unsigned long u = 0;
+    unsigned int j = 0, k = 0;
 
-int main()
-{
-  char ip[4];
-  unsigned long u;
-  unsigned int j;
-  unsigned int k;
-  char ch;
+    umask(022);
+    fd = open_read ("data");
+    if (fd == -1)
+        err (-1, "could not open file: `%s'", "data");
+    buffer_init (&b, buffer_unixread, fd, bspace, sizeof (bspace));
 
-  umask(022);
+    fdcdb = open_trunc ("data.tmp");
+    if (fdcdb == -1)
+        err (-1, "could not open file: `%s'", "data.tmp");
+    if (cdb_make_start (&cdb, fdcdb) == -1)
+        err (-1, "could not write to file: `%s'", "data.tmp");
 
-  fd = open_read("data");
-  if (fd == -1) strerr_die2sys(111,FATAL,"unable to open data: ");
-  buffer_init(&b,buffer_unixread,fd,bspace,sizeof bspace);
+    while (match)
+    {
+        ++linenum;
+        if (getln (&b, &line, &match, '\n') == -1)
+          err (-1, "could not read line");
 
-  fdcdb = open_trunc("data.tmp");
-  if (fdcdb == -1) die_datatmp();
-  if (cdb_make_start(&cdb,fdcdb) == -1) die_datatmp();
+        while (line.len)
+        {
+            ch = line.s[line.len - 1];
+            if ((ch != ' ') && (ch != '\t') && (ch != '\n'))
+                break;
+            --line.len;
+        }
+        if (!line.len)
+            continue;
 
-  while (match) {
-    ++linenum;
-    if (getln(&b,&line,&match,'\n') == -1)
-      strerr_die2sys(111,FATAL,"unable to read line: ");
+        switch (line.s[0])
+        {
+        default:
+            err (-1, "could not parse data line: invalid leading character");
 
-    while (line.len) {
-      ch = line.s[line.len - 1];
-      if ((ch != ' ') && (ch != '\t') && (ch != '\n')) break;
-      --line.len;
+        case '#':
+            break;
+
+        case ':':
+            j = byte_chr (line.s + 1, line.len - 1, ':');
+            if (j >= line.len - 1)
+                err (-1, "could not parse data line: missing colon");
+            if (ip4_scan (line.s + 1, ip) != j)
+                err (-1, "could not parse data line: malformed IP address");
+            if (!stralloc_copyb (&tmp, ip, 4))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_catb (&tmp, line.s + j + 2, line.len - j - 2))
+                err (-1, "could not allocate enough memory");
+            if (cdb_make_add (&cdb, "", 0, tmp.s, tmp.len) == -1)
+                err (-1, "could not write to file: `%s'", "data.tmp");
+            break;
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            if (!stralloc_0 (&line))
+                err (-1, "could not allocate enough memory");
+            if (!stralloc_copys (&tmp, ""))
+                err (-1, "could not allocate enough memory");
+            for (j = 0; ; j++)
+            {
+                k = scan_ulong (line.s + j, &u);
+                if (!k)
+                    break;
+
+                ch = u;
+                if (!stralloc_catb (&tmp, &ch, 1))
+                    err (-1, "could not allocate enough memory");
+
+                j += k;
+                if (line.s[j] != '.')
+                    break;
+            }
+            if (!stralloc_catb (&tmp, "\0\0\0\0", 4))
+                err (-1, "could not allocate enough memory");
+
+            tmp.len = 4;
+            if (line.s[j] == '/')
+                scan_ulong (line.s + j + 1, &u);
+            else
+                u = 32;
+
+            u = (u <= 32) ? u : 32;
+            ch = u;
+
+            if (!stralloc_catb (&tmp, &ch, 1))
+                err (-1, "could not allocate enough memory");
+            if (cdb_make_add (&cdb, tmp.s, tmp.len, "", 0) == -1)
+                err (-1, "could not write to file: `%s'", "data.tmp");
+            break;
+        }
     }
-    if (!line.len) continue;
 
-    switch(line.s[0]) {
-      default:
-	syntaxerror(": unrecognized leading character");
-      case '#':
-	break;
-      case ':':
-	j = byte_chr(line.s + 1,line.len - 1,':');
-	if (j >= line.len - 1) syntaxerror(": missing colon");
-	if (ip4_scan(line.s + 1,ip) != j) syntaxerror(": malformed IP address");
-	if (!stralloc_copyb(&tmp,ip,4)) nomem();
-	if (!stralloc_catb(&tmp,line.s + j + 2,line.len - j - 2)) nomem();
-        if (cdb_make_add(&cdb,"",0,tmp.s,tmp.len) == -1)
-          die_datatmp();
-        break;
-      case '0': case '1': case '2': case '3': case '4':
-      case '5': case '6': case '7': case '8': case '9':
-	if (!stralloc_0(&line)) nomem();
-	j = 0;
-	if (!stralloc_copys(&tmp,"")) nomem();
-	for (;;) {
-	  k = scan_ulong(line.s + j,&u);
-	  if (!k) break;
-	  ch = u;
-	  if (!stralloc_catb(&tmp,&ch,1)) nomem();
-	  j += k;
-	  if (line.s[j] != '.') break;
-	  ++j;
-	}
-	if (!stralloc_catb(&tmp,"\0\0\0\0",4)) nomem();
-	tmp.len = 4;
-	if (line.s[j] == '/')
-	  scan_ulong(line.s + j + 1,&u);
-	else
-	  u = 32;
-	if (u > 32) u = 32;
-	ch = u;
-	if (!stralloc_catb(&tmp,&ch,1)) nomem();
-        if (cdb_make_add(&cdb,tmp.s,tmp.len,"",0) == -1)
-          die_datatmp();
-	break;
-    }
-  }
-
-  if (cdb_make_finish(&cdb) == -1) die_datatmp();
-  if (fsync(fdcdb) == -1) die_datatmp();
-  if (close(fdcdb) == -1) die_datatmp(); /* NFS stupidity */
-  if (rename("data.tmp","data.cdb") == -1)
-    strerr_die2sys(111,FATAL,"unable to move data.tmp to data.cdb: ");
+    if (cdb_make_finish (&cdb) == -1)
+        err (-1, "could not write to file: `%s'", "data.tmp");
+    if (fsync (fdcdb) == -1)
+        err (-1, "could not write to file: `%s'", "data.tmp");
+    if (close (fdcdb) == -1)
+        err (-1, "could not close file: `%s'", "data.tmp"); /* NFS stupidity */
+    if (rename ("data.tmp", "data.cdb") == -1)
+        err (-1, "could not move data.tmp to data.cdb");
 
     return 0;
 }
--- rbldns.c
+++ rbldns.c
@@ -1,13 +1,37 @@
+/*
+ * rbldns.c: This file is part of the `ndjbdns' project, originally written
+ * by Dr. D J Bernstein and later released under public-domain since late
+ * December 2007 (http://cr.yp.to/distributors.html).
+ *
+ * Copyright (C) 2009 - 2012 Prasad J Pandit
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of GNU General Public License as published by Free
+ * Software Foundation; either version 2 of the license or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#include <err.h>
 #include <unistd.h>
-#include "str.h"
-#include "byte.h"
-#include "ip4.h"
-#include "open.h"
-#include "env.h"
+
+#include "dd.h"
 #include "cdb.h"
 #include "dns.h"
-#include "dd.h"
-#include "strerr.h"
+#include "env.h"
+#include "ip4.h"
+#include "str.h"
+#include "byte.h"
+#include "open.h"
 #include "response.h"
 
 static char *base;
@@ -15,101 +39,124 @@ static struct cdb c;
 static char key[5];
 static char data[100 + IP4_FMT];
 
-static int doit(char *q,char qtype[2])
+static int
+doit (char *q, char qtype[2])
 {
-  int flaga;
-  int flagtxt;
-  char ch;
-  char reverseip[4];
-  char ip[4];
-  uint32 ipnum;
-  int r;
-  uint32 dlen;
-  int i;
+    int i = 0, r = 0;
+    int flaga = 0, flagtxt = 0;
+    char ch, reverseip[4], ip[4];
 
-  flaga = byte_equal(qtype,2,DNS_T_A);
-  flagtxt = byte_equal(qtype,2,DNS_T_TXT);
-  if (byte_equal(qtype,2,DNS_T_ANY)) flaga = flagtxt = 1;
-  if (!flaga && !flagtxt) goto REFUSE;
+    uint32 ipnum = 0, dlen = 0;
 
-  if (dd(q,base,reverseip) != 4) goto REFUSE;
-  uint32_unpack(reverseip,&ipnum);
-  uint32_pack_big(ip,ipnum);
+    flaga = byte_equal (qtype, 2, DNS_T_A);
+    flagtxt = byte_equal (qtype, 2, DNS_T_TXT);
+    if (byte_equal (qtype, 2, DNS_T_ANY))
+        flaga = flagtxt = 1;
+    if (!flaga && !flagtxt)
+        goto REFUSE;
+    if (dd (q, base, reverseip) != 4)
+        goto REFUSE;
 
-  for (i = 0;i <= 24;++i) {
-    ipnum >>= i;
-    ipnum <<= i;
-    uint32_pack_big(key,ipnum);
-    key[4] = 32 - i;
-    r = cdb_find(&c,key,5);
-    if (r == -1) return 0;
-    if (r) break;
-  }
-  if (!r) { response_nxdomain(); return 1; }
+    uint32_unpack (reverseip, &ipnum);
+    uint32_pack_big (ip, ipnum);
 
-  r = cdb_find(&c,"",0);
-  if (r == -1) return 0;
-  if (r && ((dlen = cdb_datalen(&c)) >= 4)) {
-    if (dlen > 100) dlen = 100;
-    if (cdb_read(&c,data,dlen,cdb_datapos(&c)) == -1) return 0;
-  }
-  else {
-    dlen = 12;
-    byte_copy(data,dlen,"\177\0\0\2Listed $");
-  }
+    for (i = 0; i <= 24; ++i)
+    {
+        ipnum >>= i;
+        ipnum <<= i;
+        uint32_pack_big (key, ipnum);
+        key[4] = 32 - i;
 
-  if ((dlen >= 5) && (data[dlen - 1] == '$')) {
-    --dlen;
-    dlen += ip4_fmt(data + dlen,ip);
-  }
+        r = cdb_find (&c, key, 5);
+        if (r == -1)
+            return 0;
+        if (r)
+            break;
+    }
+    if (!r)
+    {
+        response_nxdomain ();
+        return 1;
+    }
 
-  if (flaga) {
-    if (!response_rstart(q,DNS_T_A,2048)) return 0;
-    if (!response_addbytes(data,4)) return 0;
-    response_rfinish(RESPONSE_ANSWER);
-  }
-  if (flagtxt) {
-    if (!response_rstart(q,DNS_T_TXT,2048)) return 0;
-    ch = dlen - 4;
-    if (!response_addbytes(&ch,1)) return 0;
-    if (!response_addbytes(data + 4,dlen - 4)) return 0;
-    response_rfinish(RESPONSE_ANSWER);
-  }
+    r = cdb_find (&c, "", 0);
+    if (r == -1)
+        return 0;
+    if (r && ((dlen = cdb_datalen (&c)) >= 4))
+    {
+        if (dlen > 100)
+            dlen = 100;
+        if (cdb_read (&c, data, dlen, cdb_datapos (&c)) == -1)
+            return 0;
+    }
+    else
+    {
+        dlen = 12;
+        byte_copy (data, dlen, "\177\0\0\2Listed $");
+    }
 
-  return 1;
+    if ((dlen >= 5) && (data[dlen - 1] == '$'))
+    {
+        --dlen;
+        dlen += ip4_fmt (data + dlen, ip);
+    }
 
+    if (flaga)
+    {
+        if (!response_rstart (q, DNS_T_A, 2048))
+            return 0;
+        if (!response_addbytes (data, 4))
+            return 0;
+        response_rfinish (RESPONSE_ANSWER);
+    }
+    if (flagtxt)
+    {
+        if (!response_rstart (q, DNS_T_TXT, 2048))
+            return 0;
 
-  REFUSE:
-  response[2] &= ~4;
-  response[3] &= ~15;
-  response[3] |= 5;
-  return 1;
+        ch = dlen - 4;
+        if (!response_addbytes (&ch, 1))
+            return 0;
+        if (!response_addbytes (data + 4, dlen - 4))
+            return 0;
+        response_rfinish (RESPONSE_ANSWER);
+    }
+    return 1;
+
+REFUSE:
+    response[2] &= ~4;
+    response[3] &= ~15;
+    response[3] |= 5;
+
+    return 1;
 }
 
-int respond(char *q,char qtype[2],char ip[4])
+int
+respond (char *q, char qtype[2], char ip[4])
 {
-  int fd;
-  int result;
+    int fd = 0;
+    int result = 0;
 
-  fd = open_read("data.cdb");
-  if (fd == -1) return 0;
-  cdb_init(&c,fd);
-  result = doit(q,qtype);
-  cdb_free(&c);
-  close(fd);
-  return result;
+    fd = open_read ("data.cdb");
+    if (fd == -1)
+        return 0;
+
+    cdb_init (&c, fd);
+    result = doit (q, qtype);
+    cdb_free (&c);
+    close (fd);
+
+    return result;
 }
 
-const char *fatal = "rbldns: fatal: ";
-const char *starting = "starting rbldns\n";
-
-void initialize(void)
+void
+initialize(void)
 {
-  char *x;
+    char *x = NULL;
 
-  x = env_get("BASE");
-  if (!x)
-    strerr_die2x(111,fatal,"$BASE not set");
-  if (!dns_domain_fromdot(&base,x,str_len(x)))
-    strerr_die2x(111,fatal,"unable to parse $BASE");
+    x = env_get("BASE");
+    if (!x)
+        err (-1, "$BASE not set");
+    if (!dns_domain_fromdot (&base, x, str_len (x)))
+        err (-1, "unable to parse $BASE");
 }
