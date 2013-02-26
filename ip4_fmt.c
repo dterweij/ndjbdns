d31fea267289f3cf24edfdc3f2a1460ff3d25273
P J P <pj.pandit@yahoo.co.in>
2013-02-26 20:06:36 +0530

    Update ip4_fmt routine to be concise.
    Changed ip4_fmt routine to be easy to follow and concise.

--- ip4_fmt.c
+++ ip4_fmt.c
@@ -3,7 +3,7 @@
  * by Dr. D J Bernstein and later released under public-domain since late
  * December 2007 (http://cr.yp.to/distributors.html).
  *
- * Copyright (C) 2009 - 2012 Prasad J Pandit
+ * Copyright (C) 2009 - 2013 Prasad J Pandit
  *
  * This program is a free software; you can redistribute it and/or modify
  * it under the terms of GNU General Public License as published by Free
@@ -26,36 +26,20 @@
 unsigned int
 ip4_fmt (char *s, const char ip[4])
 {
-    unsigned int i = 0, len = 0;
+    unsigned int i = 0, j = 0, len = 0;
 
-    i = fmt_ulong (s, (unsigned long)(unsigned char)ip[0]);
-    len += i;
-    if (s)
-        s += i;
-    if (s)
-        *s++ = '.';
-    ++len;
-
-    i = fmt_ulong (s, (unsigned long)(unsigned char)ip[1]);
-    len += i;
-    if (s)
-        s += i;
-    if (s)
-        *s++ = '.';
-    ++len;
-
-    i = fmt_ulong (s, (unsigned long)(unsigned char)ip[2]);
-    len += i;
-    if (s)
-        s += i;
-    if (s)
-        *s++ = '.';
-    ++len;
-
-    i = fmt_ulong (s, (unsigned long)(unsigned char)ip[3]);
-    len += i;
-    if (s)
-        s += i;
+    while (j < 4)
+    {
+        i = fmt_ulong (s, (unsigned long)(unsigned char)ip[j++]);
+        len += i;
+        if (s)
+            s += i;
+        if (j == 4)
+            break;
+        if (s)
+            *s++ = '.';
+        ++len;
+    }
 
     return len;
 }
