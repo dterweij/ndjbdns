4f65e3f0d38435ecddde5f0f02d6cb978cd0cd97
P J P <pj.pandit@yahoo.co.in>
2013-11-24 00:17:50 +0530

    Improve root server's log structure
    Changed 'qlog.c' routines to improve tinydns' and its cousin
    server's log structure for better comprehension. Added timestamps,
    query number; And replaced hexadecimal values with human readable
    decimals.
    
    Also did little source formatting to make it more readable.

--- qlog.h
+++ qlog.h
@@ -1,8 +1,31 @@
+/*
+ * qlog.h: This file is part of the `djbdns' project, originally written
+ * by Dr. D J Bernstein and later released under public-domain since late
+ * December 2007 (http://cr.yp.to/distributors.html).
+ *
+ * Copyright (C) 2009 - 2013 Prasad J Pandit
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of GNU General Public License as published by Free
+ * Software Foundation; either version 2 of the license or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #ifndef QLOG_H
 #define QLOG_H
 
 #include "uint16.h"
 
-extern void qlog(const char *,uint16,const char *,const char *,const char *,const char *);
+extern void qlog(unsigned long long, const char *, uint16,
+                  const char *, const char *, const char *, const char *);
 
 #endif
